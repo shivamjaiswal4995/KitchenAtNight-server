09ae8dec3ed70e14b593a27bbce7d66518f5086e
shivam jaiswal <shivam.jaiswal4995@gmail.com>
2018-12-17 01:57:34 +0530

    mmmm

--- server/bin/www.js
+++ server/bin/www.js
@@ -6,48 +6,48 @@ const port = (process.env.PORT || 3000);
 server.listen(port, () => {
 	console.log(` server is listening on ${port} `);
 })
-io.on('connection', socket => {
-	// sockets.add(socket);
-	// console.log("Socket: ", socket);
-	console.log(`Socket ${socket.id} added`);
+// io.on('connection', socket => {
+// 	// sockets.add(socket);
+// 	// console.log("Socket: ", socket);
+// 	console.log(`Socket ${socket.id} added`);
 	
-	socket.on('signin', data => {
-		console.log("Data recieved in SIGNIN event ", data, socket.id);
-		socket.join(data.userEmail); 
-	});
+// 	socket.on('signin', data => {
+// 		console.log("Data recieved in SIGNIN event ", data, socket.id);
+// 		socket.join(data.userEmail); 
+// 	});
 
-	socket.on('signup', data => {
-		console.log("Data recieved in SIGNUP event ", data, socket.id);
-		socket.join(data.userEmail);
-	});
+// 	socket.on('signup', data => {
+// 		console.log("Data recieved in SIGNUP event ", data, socket.id);
+// 		socket.join(data.userEmail);
+// 	});
 
-	socket.on('share', data => {
-		const email = data.receipient.split(',')[1].trim();
-		console.log('Data received on SHARE event', email, socket.id);
-		const response = {
-			title: data.title,
-			from: data.sender
-		}
+// 	socket.on('share', data => {
+// 		const email = data.receipient.split(',')[1].trim();
+// 		console.log('Data received on SHARE event', email, socket.id);
+// 		const response = {
+// 			title: data.title,
+// 			from: data.sender
+// 		}
 		
-		if(io.sockets.adapter.rooms[email]) {
-			// console.log('room exists');
-			io.sockets.in(email).emit('notify', response);
-		} else {
-			let obj = {
-				content: `Note titled "${data.title}" has been shared to you by user ${data.sender}`
-			}
-			notificationController.addNotification(data.idOfSharedUser, obj, (err, savedNotification) => {
-				if(err) {
-					logger.error('error occurred while saving notification: ', err);
-				} else {
-					console.log('Notification saved: ', savedNotification);
-				}
-			});
-			// console.log('room does not exist!');
-		}
-	});
+// 		if(io.sockets.adapter.rooms[email]) {
+// 			// console.log('room exists');
+// 			io.sockets.in(email).emit('notify', response);
+// 		} else {
+// 			let obj = {
+// 				content: `Note titled "${data.title}" has been shared to you by user ${data.sender}`
+// 			}
+// 			notificationController.addNotification(data.idOfSharedUser, obj, (err, savedNotification) => {
+// 				if(err) {
+// 					logger.error('error occurred while saving notification: ', err);
+// 				} else {
+// 					console.log('Notification saved: ', savedNotification);
+// 				}
+// 			});
+// 			// console.log('room does not exist!');
+// 		}
+// 	});
 
-    socket.on('disconnect', () => {
-		console.log(`Deleting socket: ${socket.id}`);
-	});
-});
+//     socket.on('disconnect', () => {
+// 		console.log(`Deleting socket: ${socket.id}`);
+// 	});
+// });
