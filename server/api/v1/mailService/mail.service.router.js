fb06e62a2ee4b51e144f1274a9bcc770e0af037b
shivam jaiswal <shivam.jaiswal4995@gmail.com>
2019-01-22 01:34:37 +0530

    socket................

--- server/api/v1/mailService/mail.service.router.js
+++ server/api/v1/mailService/mail.service.router.js
@@ -1,152 +1,152 @@
-const nodemailer = require('nodemailer');
-const config = require('../../../config');
-const logger = require('../../../logger');
-const router = require('express').Router();
+// const nodemailer = require('nodemailer');
+// const config = require('../../../config');
+// const logger = require('../../../logger');
+// const router = require('express').Router();
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'KitchenAtNight24.7@gmail.com',
-        pass: 'shivnarayanjaiswal'
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'KitchenAtNight24.7@gmail.com',
+//         pass: 'shivnarayanjaiswal'
+//     }
+// });
 
-router.post('/confirmationMail', (req,res) => {
+// router.post('/confirmationMail', (req,res) => {
 
     
-        const receiversName = req.body.receiversName;
-        const orderId = req.body.orderId;
-        const to = req.body.to;
-        const message= `<strong>Hello ${receiversName}</strong>, <br>Greetings to you.<br><br><p> We thought you'd like to know that your order with orderId ${orderId} has been confirmed,
-        <br> and is in preparation phase in Kitchen.<br><br>We will love to improve your experience. Please provide feedback and suggestions.<br>
-        <br>Have a great meal and good time.<br><br>With Regards,<br>BiteAtNightTeam<br>`;
+//         const receiversName = req.body.receiversName;
+//         const orderId = req.body.orderId;
+//         const to = req.body.to;
+//         const message= `<strong>Hello ${receiversName}</strong>, <br>Greetings to you.<br><br><p> We thought you'd like to know that your order with orderId ${orderId} has been confirmed,
+//         <br> and is in preparation phase in Kitchen.<br><br>We will love to improve your experience. Please provide feedback and suggestions.<br>
+//         <br>Have a great meal and good time.<br><br>With Regards,<br>BiteAtNightTeam<br>`;
     
-        let mailOptions = {
-            from: 'KitchenAtNight24.7@gmail.com',
-            to: to,
-            subject: `<strong>Order Confirmed :</strong> Order with orderId ${orderId}`,
-            html: message
-        };
+//         let mailOptions = {
+//             from: 'KitchenAtNight24.7@gmail.com',
+//             to: to,
+//             subject: `<strong>Order Confirmed :</strong> Order with orderId ${orderId}`,
+//             html: message
+//         };
     
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) {
-                if(config.logging) {
-                    logger.error("Error occurred while sending notification mail: ", err);
-                }
-                console.log("Error occurred while sending notification mail: ", err);
-                res.status(500).send(err);
-            } else {
-                console.log('Confirmation mail has been sent successfully!');
+//         transporter.sendMail(mailOptions, (err, info) => {
+//             if(err) {
+//                 if(config.logging) {
+//                     logger.error("Error occurred while sending notification mail: ", err);
+//                 }
+//                 console.log("Error occurred while sending notification mail: ", err);
+//                 res.status(500).send(err);
+//             } else {
+//                 console.log('Confirmation mail has been sent successfully!');
             
-                res.status(200).send('Confirmation mail has been sent successfully!');
-            }
-        });
+//                 res.status(200).send('Confirmation mail has been sent successfully!');
+//             }
+//         });
     
-});
+// });
 
-router.post('/deliveryMail', (req,res) => {
+// router.post('/deliveryMail', (req,res) => {
     
-        const receiversName = req.body.receiversName;
-        const orderId = req.body.orderId;
-        const to = req.body.to;
-        const message = `<strong>Dear ${receiversName}</strong>, <br>Greetings to you.<br><br>
-        <p> Your order with orderId ${orderId} has been delivered.<br><br>Please provide your valuable feedback in feedback section of app<br>
-        <br>We are dedicated towards enhancing your overall experience with BiteAtNight food delivery service.<br>
-        <br>Have a great meal and good time.<br>
-        <br><br>With Regards,<br>BiteAtNight Team`;
+//         const receiversName = req.body.receiversName;
+//         const orderId = req.body.orderId;
+//         const to = req.body.to;
+//         const message = `<strong>Dear ${receiversName}</strong>, <br>Greetings to you.<br><br>
+//         <p> Your order with orderId ${orderId} has been delivered.<br><br>Please provide your valuable feedback in feedback section of app<br>
+//         <br>We are dedicated towards enhancing your overall experience with BiteAtNight food delivery service.<br>
+//         <br>Have a great meal and good time.<br>
+//         <br><br>With Regards,<br>BiteAtNight Team`;
         
-        let mailOptions = {
-            from: 'KitchenAtNight24.7@gmail.com',
-            to: to,
-            subject: `<strong>Order Delivered : </strong>Order with orderId ${orderId}`,
-            html: message
-        };
+//         let mailOptions = {
+//             from: 'KitchenAtNight24.7@gmail.com',
+//             to: to,
+//             subject: `<strong>Order Delivered : </strong>Order with orderId ${orderId}`,
+//             html: message
+//         };
     
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) {
-                if(config.logging) {
-                    logger.error("Error occurred while sending delivery mail: ", err);
-                }
-                console.log("Error occurred while sending delivery mail: ", err);
-                res.status(500).send(err);
-            } else {
-                console.log('Delivery mail has been sent successfully!');
-                res.status(200).send('Delivery mail has been sent successfully!');
-            }
-        });
+//         transporter.sendMail(mailOptions, (err, info) => {
+//             if(err) {
+//                 if(config.logging) {
+//                     logger.error("Error occurred while sending delivery mail: ", err);
+//                 }
+//                 console.log("Error occurred while sending delivery mail: ", err);
+//                 res.status(500).send(err);
+//             } else {
+//                 console.log('Delivery mail has been sent successfully!');
+//                 res.status(200).send('Delivery mail has been sent successfully!');
+//             }
+//         });
         
-});
+// });
 
-router.post('/referralMail', (req,res) => {
+// router.post('/referralMail', (req,res) => {
     
-        const receiversName = req.body.receiversName; /*receivers name is name of guy whose referral code have been used*/
-        const referralCode = req.body.referralCode;
-        const referralUsedBy = req.body.referralUsedBy;
-        const to = req.body.to;
-        const message= `<strong>Hello ${receiversName}</strong>, <br>Wuhooo!!!<br><br>
-        <p> Congratulations for earning referral coupon <strong>KitchenAtNight30</strong>!! <br>
-        Your friend ${referralUsedBy} has used your referral code ${referralCode}<br>
-        <br>You can use coupon code KitchenAtNight30 to avail <strong>30% off upto 100 rupees</strong> on your next order.<br>
-        <br>Keep referring to friends and earn more referral coupon.<br>
-        <br>Have a great meal and good time.<br><br>With Regards,<br>KitchenAtNightTeam<br>`;
+//         const receiversName = req.body.receiversName; /*receivers name is name of guy whose referral code have been used*/
+//         const referralCode = req.body.referralCode;
+//         const referralUsedBy = req.body.referralUsedBy;
+//         const to = req.body.to;
+//         const message= `<strong>Hello ${receiversName}</strong>, <br>Wuhooo!!!<br><br>
+//         <p> Congratulations for earning referral coupon <strong>KitchenAtNight30</strong>!! <br>
+//         Your friend ${referralUsedBy} has used your referral code ${referralCode}<br>
+//         <br>You can use coupon code KitchenAtNight30 to avail <strong>30% off upto 100 rupees</strong> on your next order.<br>
+//         <br>Keep referring to friends and earn more referral coupon.<br>
+//         <br>Have a great meal and good time.<br><br>With Regards,<br>KitchenAtNightTeam<br>`;
     
-        let mailOptions = {
-            from: 'KitchenAtNight24.7@gmail.com',
-            to: to,
-            subject: `<strong>Referral Coupon Earned: KitchenAtNight30 </strong>`,
-            html: message
-        };
+//         let mailOptions = {
+//             from: 'KitchenAtNight24.7@gmail.com',
+//             to: to,
+//             subject: `<strong>Referral Coupon Earned: KitchenAtNight30 </strong>`,
+//             html: message
+//         };
     
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) {
-                if(config.logging) {
-                    logger.error("Error occurred while sending notification mail: ", err);
-                }
-                 res.status(500).send(err);  
-                console.log("Error occurred while sending notification mail: ", err);
-            } else {
-                console.log('Referral mail has been sent successfully!');
-                res.status(200).send('Referral mail has been sent successfully!');
-            }
-        });    
-});
+//         transporter.sendMail(mailOptions, (err, info) => {
+//             if(err) {
+//                 if(config.logging) {
+//                     logger.error("Error occurred while sending notification mail: ", err);
+//                 }
+//                  res.status(500).send(err);  
+//                 console.log("Error occurred while sending notification mail: ", err);
+//             } else {
+//                 console.log('Referral mail has been sent successfully!');
+//                 res.status(200).send('Referral mail has been sent successfully!');
+//             }
+//         });    
+// });
 
-router.post('verificationMail', (req,res) => {
+// router.post('verificationMail', (req,res) => {
     
-        const receiversName = req.body.receiversName;
-        const to = req.body.to;
-        const message= `<strong>Hello ${receiversName}</strong>, <br>Your registration is almost complete<br><br>
-        <p> <br>
-        Your friend ${referralUsedBy} has used your referral code ${referralCode}<br>
-        <br>You can use coupon code KitchenAtNight30 to avail <strong>30% off upto 100 rupees</strong> on your next order.<br>
-        <br>Keep referring to friends and earn more referral coupon.<br>
-        <br>Have a great meal and good time.<br><br>With Regards,<br>KitchenAtNightTeam<br>`;
+//         const receiversName = req.body.receiversName;
+//         const to = req.body.to;
+//         const message= `<strong>Hello ${receiversName}</strong>, <br>Your registration is almost complete<br><br>
+//         <p> <br>
+//         Your friend ${referralUsedBy} has used your referral code ${referralCode}<br>
+//         <br>You can use coupon code KitchenAtNight30 to avail <strong>30% off upto 100 rupees</strong> on your next order.<br>
+//         <br>Keep referring to friends and earn more referral coupon.<br>
+//         <br>Have a great meal and good time.<br><br>With Regards,<br>KitchenAtNightTeam<br>`;
     
-        let mailOptions = {
-            from: 'KitchenAtNight24.7@gmail.com',
-            to: details.to,
-            subject: `<strong>Referral Coupon Earned: KitchenAtNight30 </strong>`,
-            html: message
-        };
+//         let mailOptions = {
+//             from: 'KitchenAtNight24.7@gmail.com',
+//             to: details.to,
+//             subject: `<strong>Referral Coupon Earned: KitchenAtNight30 </strong>`,
+//             html: message
+//         };
     
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) {
-                if(config.logging) {
-                    logger.error("Error occurred while sending notification mail: ", err);
-                }
-                console.log("Error occurred while sending notification mail: ", err);
-            } else {
-                console.log('Confirmation mail has been sent successfully!');
-            }
-        });
-    }
+//         transporter.sendMail(mailOptions, (err, info) => {
+//             if(err) {
+//                 if(config.logging) {
+//                     logger.error("Error occurred while sending notification mail: ", err);
+//                 }
+//                 console.log("Error occurred while sending notification mail: ", err);
+//             } else {
+//                 console.log('Confirmation mail has been sent successfully!');
+//             }
+//         });
+//     }
     
-);
+// );
 
-module.exports = {
-    sendDeliveryMail,
-    sendConfirmationMail,
-    sendReferralMail,
-    sendVerificationMail
-}
+// module.exports = {
+//     sendDeliveryMail,
+//     sendConfirmationMail,
+//     sendReferralMail,
+//     sendVerificationMail
+// }
 
